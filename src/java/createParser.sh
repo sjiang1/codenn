2e3614494a247e3adb9cb836058eba4133427e4f
Siyuan Jiang <sjiang1@nd.edu>
2018-05-22 13:17:07 -0400

    updated the script to Java parser

--- src/java/createParser.sh
+++ src/java/createParser.sh
@@ -4,4 +4,8 @@ if [ ! -f JavaLexer.g4 ]; then
     curl -O https://raw.githubusercontent.com/antlr/grammars-v4/master/java/JavaLexer.g4
 fi
 
-java -Xmx500M -cp "/home/siyuan_jiang_827/antlr-4.5.3-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python2 JavaLexer.g4
+if [ ! -f antlr-4.5.3-complete.jar ]; then
+    curl -O http://www.antlr.org/download/antlr-4.5.3-complete.jar
+fi
+
+java -Xmx500M -cp "antlr-4.5.3-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python2 JavaLexer.g4
