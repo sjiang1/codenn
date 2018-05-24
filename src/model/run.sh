6bcf5c43c0055bbe60cb87d95e744a8f7d3114b4
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2018-05-24 13:17:04 +0000

    updated the run.sh to run main.lua

--- src/model/run.sh
+++ src/model/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 LANGUAGE=$1
+OUTDIR=$2
 GPUIDX=1
 BEAMSIZE=10
 
 # Run Training
-th main.lua -dev_ref_file $CODENN_DIR/data/stackoverflow/${LANGUAGE}/dev/ref.txt -gpuidx $GPUIDX -language $1
+th main.lua -gpuidx $GPUIDX -language $1 -outdir ${OUTDIR}
 
 # Run prediction
 th predict.lua -encoder ${LANGUAGE}.encoder -decoder ${LANGUAGE}.decoder -beamsize $BEAMSIZE -gpuidx $GPUIDX -language ${LANGUAGE}
