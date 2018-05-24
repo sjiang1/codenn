27f1135573e1bc68380013f12cbcf56c0fd4d796
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2018-05-24 15:01:55 +0000

    added a print for finishing saving models

--- src/model/main.lua
+++ src/model/main.lua
@@ -218,6 +218,7 @@ function save_models()
 	-- torch.save(opt.language .. '.decoder', decoder.cells[1]) -- save only the decoder cell
 	torch.save(opt.outdir .. opt.language .. '.encoder' .. '.e' .. tostring(epoch), encoder.cell) -- save the whole encoder
 	torch.save(opt.outdir .. opt.language .. '.decoder' .. '.e' .. tostring(epoch), decoder.cells[1]) -- save only the decoder cell
+	print('saving models: ... done')
 end
 
 main()
